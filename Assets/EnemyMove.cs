99820d9f37284328ba521cd2669b16179f3a84d4
TAKAHASHIYUTOGIT <134475821+TAKAHASHIYUTOGIT@users.noreply.github.com>
2023-06-12 17:00:11 +0900

    プレイヤーの動きなど色々

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    SpriteRenderer _sr;
+    [SerializeField] float _speed;
+    float _dir = 1;
+    [SerializeField] Transform[] _returnPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+        _sr.flipX = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > _returnPos[0].position.x)
+        {
+            _dir = -1;
+            _sr.flipX = false;
+        }
+        else if (transform.position.x < _returnPos[1].position.x)
+        {
+            _dir = 1;
+            _sr.flipX = true;
+        }
+        Debug.Log(_dir);
+
+    }
+}
--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,34 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    Rigidbody2D _rb;
+    Rigidbody2D _rb = default;
+    SpriteRenderer _sprite = default;
     /// <summary>左右移動する力</summary>
     [SerializeField] float m_movePower = 5f;
     /// <summary>ジャンプする力</summary>
     [SerializeField] float m_jumpPower = 15f;
-    /// <summary>入力に応じて左右を反転させるかどうかのフラグ</summary>
-    [SerializeField] bool m_flipX = false;
     //<summary>水平方向の入力値
     float m_h;
     float m_scaleX;
-
     // Start is called before the first frame update
+    int count = 0;
+    bool m_isGround = false;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per 
     private void Update()
     {//入力を受け取る
-        m_h = Input.GetAxisRaw("Horizontal"); 
-        
+        m_h = Input.GetAxisRaw("Horizontal");
+
         Vector2 velocity = _rb.velocity;
         velocity.x = m_h * m_movePower;
         _rb.velocity = velocity;
+
+        // 各種入力を受け取る
+        if (Input.GetButtonDown("Jump") && count < 2)
+        {
+            _rb.AddForce(Vector2.up * m_jumpPower, ForceMode2D.Impulse);
+            count++;
+        }
+    }
+    private object AddForce(float m_jumpPower)
+    {
+        throw new NotImplementedException();
     }
 
     private void FixedUpdate()
@@ -53,5 +69,10 @@ public class PlayerMove : MonoBehaviour
         {
             this.transform.localScale = new Vector3(-1 * Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        m_isGround = true;
+        count = 0;
     }
 }
